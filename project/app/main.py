25adfe8cfbfd0b1b0d9fbd8004b7b87c10d157f7
Joseph Priest <josephplpriest@gmail.com>
2022-10-28 06:27:10 -0900

    Adding postgres db + tortoise ORM

--- project/app/main.py
+++ project/app/main.py
@@ -1,10 +1,21 @@
 """ ~/app/main.py"""
 
 from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import get_settings, Settings
 
+
 app = FastAPI()
+
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
+
 
 @app.get("/marco")
 async def polo(settings: Settings = Depends(get_settings)):
